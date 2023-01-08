e699d1017d23a2361459f9e234f8a8ebaad60085
MariadeFatima624 <mariadefatimaudi2004@gmail.com>
2023-01-08 14:02:43 -0300

    Laboratório 5

--- LabPP5/racionais.c
+++ LabPP5/racionais.c
@@ -5,37 +5,69 @@ Lista de exercícios - Médio 2
 */
 
 #include <stdio.h>
-#include <math.h>
 
-float S(int n);
+int calc_mdc(int a,int b);
+void ajustaQ(int *p, int *q, int mdc);
 
 typedef struct numeros{
     int numerador, denominador;
 }s_numeros;
 
 int main(void){
-    printf("Informe dois números racionais no formato numerador1 denominador1   numerador2 denominador2: ");
     s_numeros Q1, Q2;
-    scanf("%d %d    %d %d", &Q1.numerador, &Q1.denominador, &Q2.numerador, &Q2.denominador);
-    mdc1 = calc_mdc(Q1.numerador, Q1.denominador);
-    mdc2 = calc_mdc(Q2.numerador, Q2.denominador);
-    Q1.numerador= Q1.numerador/mdc1;
-    Q1.denominador= Q1.denominador/mdc1;
-    Q2.numerador= Q2.numerador/mdc2;
-    Q2.denominador= Q2.denominador/mdc2;
+    printf("Informe o primeiro número racional no formato numerador1 denominador1:\n");
+    scanf("%d %d", &Q1.numerador, &Q1.denominador);
+    printf("Informe o segundo número racional no formato numerador2 denominador2:\n");
+    scanf("%d %d", &Q2.numerador, &Q2.denominador);
+    int mdc1 = calc_mdc(Q1.numerador, Q1.denominador);
+    int mdc2 = calc_mdc(Q2.numerador, Q2.denominador);
+    ajustaQ(&Q1.numerador, &Q1.denominador,mdc1);
+    ajustaQ(&Q2.numerador, &Q2.denominador,mdc2);
+
+    s_numeros soma, subtracao, multiplicacao, divisao;
+    int d = Q1.denominador*Q2.denominador;
+    soma.numerador = (Q1.numerador*Q2.denominador)+(Q2.numerador*Q1.denominador);
+    soma.denominador = d;
+    subtracao.numerador = (Q1.numerador*Q2.denominador)-(Q2.numerador*Q1.denominador);
+    subtracao.denominador = d;
+    multiplicacao.numerador = Q1.numerador*Q2.numerador;
+    multiplicacao.denominador = d;
+    divisao.numerador = Q1.numerador*Q2.denominador;
+    divisao.denominador = Q1.denominador*Q2.numerador;
+
+    int mdcSoma = calc_mdc(soma.numerador, soma.denominador);
+    int mdcSub = calc_mdc(subtracao.numerador, subtracao.denominador);
+    int mdcMult = calc_mdc(multiplicacao.numerador, multiplicacao.denominador);
+    int mdcDiv = calc_mdc(divisao.numerador, divisao.denominador);
+
+    ajustaQ(&soma.numerador, &soma.denominador,mdcSoma);
+    ajustaQ(&subtracao.numerador, &subtracao.denominador,mdcSub);
+    ajustaQ(&multiplicacao.numerador, &multiplicacao.denominador,mdcMult);
+    ajustaQ(&divisao.numerador, &divisao.denominador,mdcDiv);
 
 
+    printf("%d/%d ",Q1.numerador, Q1.denominador);
+    printf("%d/%d ",Q2.numerador, Q2.denominador);
+    printf("%d/%d ",soma.numerador, soma.denominador);
+    printf("%d/%d ",subtracao.numerador, subtracao.denominador);
+    printf("%d/%d ",multiplicacao.numerador, multiplicacao.denominador);
+    printf("%d/%d",divisao.numerador, divisao.denominador);
+    printf("\n");
 
-    float aprox = S(n);
-    printf("%f", S(n));
     return 0;
 }
 
-float S(int n){
-    if(n==0) return 4;
-    return (S(n-1)+4*(pow((-1),n)/(2*n+1)));
-}
+void ajustaQ(int *n, int *d, int mdc){
+    *n= *n/mdc;
+    *d= *d/mdc;
+    
+    if(*n == 0) *d=1;
 
+    if(*d < 0){
+        *d *= (-1);
+        *n *= (-1);
+    }
+}
 
 int calc_mdc(int a,int b){
     if(b==0) return a;
